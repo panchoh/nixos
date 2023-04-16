63020ae1530932889ac65ae51a8d6ef2385909bf
pancho horrillo <pancho@pancho.name>
2023-04-16 17:30:49 +0200

    Limit amount of generations in the boot menu

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.consoleMode = "keep";
   boot.loader.systemd-boot.memtest86.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.kernelPackages = pkgs.linuxPackages_latest;
