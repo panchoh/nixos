43546136d290955c0593e8797a8b2aef0219eee6
pancho horrillo <pancho@pancho.name>
2023-04-20 15:09:09 +0200

    feat: enable fail2ban service

--- configuration.nix
+++ configuration.nix
@@ -304,6 +304,9 @@ in {
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # Enable the fail2ban service
+  services.fail2ban.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
