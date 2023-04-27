3b4d7afe151e273af379f4e3795e47adf4096453
pancho horrillo <pancho@pancho.name>
2023-04-27 07:59:04 +0200

    doc: drop a lot of comments

--- configuration.nix
+++ configuration.nix
@@ -16,20 +16,16 @@ in {
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
-  #boot.loader.timeout = null;
   boot.loader.timeout = 0;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.consoleMode = "keep";
   boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot";
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = ["quiet" "loglevel=3" "systemd.show_status=auto" "udev.log_level=3"];
   boot.initrd.kernelModules = [ "i915" "btrfs" ];
   boot.initrd.verbose = false;
-  # EXPERIMENTAL:
   boot.initrd.systemd.enable = true;
   boot.consoleLogLevel = 0;
   boot.plymouth.enable = true;
@@ -42,22 +38,13 @@ in {
 
   networking.usePredictableInterfaceNames = false;
   networking.enableIPv6 = false;
-  networking.hostName = "helium"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "helium";
 
   networking.useDHCP = false;
   networking.interfaces.eno1.useDHCP = true;
   networking.useNetworkd = true;
   networking.search = ["home"];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  # networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "Europe/Madrid";
 
   hardware.opengl = {
@@ -110,27 +97,14 @@ in {
 
   services.fwupd.enable = true;
 
-  # Enable the X11 windowing system.
-  #services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  #services.xserver = {
-  #  xkbModel = "hhk";
-  #  layout = "us,us";
-  #  xkbVariant = "altgr-intl,dvorak-alt-intl";
-  #  xkbOptions = "compose:sclk,grp:shifts_toggle";
-  #  videoDrivers = [ "intel" ];
-  #};
   # Configure keymap in X11
   services.xserver = {
     xkbModel = "hhk";
     layout = "us,us";
     xkbVariant = "altgr-intl,dvorak-alt-intl";
     xkbOptions = "compose:sclk,grp:shifts_toggle";
+  #  videoDrivers = [ "intel" ];
   };
 
   # Enable CUPS to print documents.
@@ -144,17 +118,8 @@ in {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
     wireplumber.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # handled by stylix
   # https://github.com/danth/stylix/blob/master/stylix/nixos/fonts.nix
@@ -286,7 +251,6 @@ in {
   virtualisation.podman.enable = true;
   virtualisation.podman.dockerCompat = true;
 
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh = {
@@ -304,14 +268,7 @@ in {
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Enable the fail2ban service
   services.fail2ban.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  system.stateVersion = "22.11";
 }
