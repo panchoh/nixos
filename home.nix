492c1cb15daed240d7d7a265b97725857e016606
pancho horrillo <pancho@pancho.name>
2023-05-20 10:59:39 +0200

    feat(home): enable yt-dlp

--- home.nix
+++ home.nix
@@ -464,6 +464,17 @@ in {
     };
   };
 
+  programs.yt-dlp = {
+    enable = true;
+    settings = {
+      embed-thumbnail = true;
+      embed-subs = true;
+      sub-langs = "all";
+      downloader = "aria2c";
+      downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
+    };
+  };
+
   programs.mpv = {
     enable = true;
     bindings = {
