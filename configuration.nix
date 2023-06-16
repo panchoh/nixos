649ef61cefa2e30a1232efbba7b91c5057c161b9
pancho horrillo <pancho@pancho.name>
2023-06-16 16:11:26 +0200

    fix(configuration): punch transmission port in fw

--- configuration.nix
+++ configuration.nix
@@ -291,6 +291,7 @@
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [51413];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
