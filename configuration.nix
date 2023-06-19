a3afe61891a6303be509b66573ff72a6306d8af4
pancho horrillo <pancho@pancho.name>
2023-06-19 10:31:17 +0200

    feat(configuration): only ed25519 keys, no rsa

--- configuration.nix
+++ configuration.nix
@@ -292,6 +292,12 @@
   programs.mosh.enable = true;
   services.openssh = {
     enable = true;
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
