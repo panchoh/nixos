c1ec888b5c616d0d8152d9515550e5d87a0a82f1
pancho horrillo <pancho@pancho.name>
2023-08-28 08:55:54 +0200

    feat(home): fully configure virt-manager

--- home.nix
+++ home.nix
@@ -234,6 +234,35 @@
     userDirs.download = "${config.home.homeDirectory}/incoming";
   };
 
+  # https://github.com/virt-manager/virt-manager/blob/main/data/org.virt-manager.virt-manager.gschema.xml
+  dconf.settings = {
+    "org/virt-manager/virt-manager" = {
+      xmleditor-enabled = true;
+    };
+    "org/virt-manager/virt-manager/confirm" = {
+      forcepoweroff = false;
+    };
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = ["qemu:///system"];
+      uris = ["qemu:///system"];
+    };
+    "org/virt-manager/virt-manager/new-vm" = {
+      firmware = "uefi";
+    };
+    "org/virt-manager/virt-manager/stats" = {
+      enable-disk-poll = true;
+      enable-net-poll = true;
+      enable-memory-poll = true;
+    };
+    "org/virt-manager/virt-manager/vmlist-fields" = {
+      disk-usage = true;
+      network-traffic = true;
+      cpu-usage = true;
+      host-cpu-usage = true;
+      memory-usage = true;
+    };
+  };
+
   programs.fuzzel = {
     enable = true;
     settings = {
