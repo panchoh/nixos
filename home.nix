f1fdba2f48ad5c7ac27c01b7db7bae4ebc9a3ee3
pancho horrillo <pancho@pancho.name>
2023-12-04 22:40:15 +0100

    feat(home): adjust scdaemonSettings on GnuPG
    Shamelessly stolen from https://github.com/laetitiae/nixos-config/blob/05c8372dd34f7f4e238b9b5263650c2ac8a263e1/configuration.nix#L206

--- home.nix
+++ home.nix
@@ -520,6 +520,10 @@
 
   programs.gpg = {
     enable = true;
+    scdaemonSettings = {
+      disable-ccid = true; # Play nice with yubikey https://ludovicrousseau.blogspot.com/2019/06/gnupg-and-pcsc-conflicts.html
+      reader-port = "Yubico YubiKey CCID 00 00"; # Get this value with pcsc_scan
+    };
     homedir = "${config.xdg.dataHome}/gnupg";
   };
 
