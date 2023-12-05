5aa22d364da47e678a2c9af1f545fe9657398414
pancho horrillo <pancho@pancho.name>
2023-12-05 20:25:06 +0100

    feat(home): further firefox tweaking
    See
    https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265

--- home.nix
+++ home.nix
@@ -471,6 +471,7 @@
     plugins = [pkgs.obs-studio-plugins.wlrobs];
   };
 
+  # https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265
   programs.firefox = {
     enable = true;
     package = pkgs.firefox.override {
@@ -479,18 +480,66 @@
     profiles.default.id = 0;
     policies = {
       # https://mozilla.github.io/policy-templates
+      # about:policies#documentation
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
       DNSOverHTTPS.Enabled = 0;
       DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+      DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
       Homepage.StartPage = "none";
       NewTabPage = false;
       OfferToSaveLogins = false;
       OfferToSaveLoginsDefault = false;
       OverrideFirstRunPage = "";
-      Preferences = {
+      OverridePostUpdatePage = "";
+      SearchBar = "unified"; # alternative: "separate"
+      Preferences = let
+        lock-true = {
+          Value = true;
+          Status = "locked";
+        };
+        lock-false = {
+          Value = false;
+          Status = "locked";
+        };
+        lock-strict = {
+          Value = "strict";
+          Status = "locked";
+        };
+      in {
         "browser.display.use_document_fonts" = 1; # Force stylix fonts on all pages
         "browser.sessionstore.resume_from_crash" = false;
         "dom.security.https_only_mode" = true;
         "dom.security.https_only_mode_ever_enabled" = true;
+        "browser.contentblocking.category" = lock-strict;
+        "extensions.pocket.enabled" = lock-false;
+        "extensions.screenshots.disabled" = lock-true;
+        "browser.topsites.contile.enabled" = lock-false;
+        "browser.formfill.enable" = lock-false;
+        "browser.search.suggest.enabled" = lock-false;
+        "browser.search.suggest.enabled.private" = lock-false;
+        "browser.urlbar.suggest.searches" = lock-false;
+        "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
       };
     };
   };
