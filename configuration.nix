7e93f87a9489a1f4008efe3e377a921babf3c599
pancho horrillo <pancho@pancho.name>
2023-12-23 23:37:17 +0100

    feat(cfg): set energy_perf_policy at boot

--- configuration.nix
+++ configuration.nix
@@ -143,6 +143,23 @@
     };
   };
 
+  # https://docs.kernel.org/admin-guide/pm/intel_epb.html
+  # https://bbs.archlinux.org/viewtopic.php?id=270199
+  # https://stackoverflow.com/questions/58243712/how-to-install-systemd-service-on-nixos
+  # cat /sys/devices/system/cpu/cpu*/power/energy_perf_bias
+  systemd.services.epb = {
+    enable = true;
+    description = "Run x86_energy_perf_policy at boot";
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStartPre = "${lib.getExe' pkgs.kmod "modprobe"} msr";
+      ExecStart = "${lib.getExe' pkgs.linuxPackages_latest.x86_energy_perf_policy "x86_energy_perf_policy"} performance";
+      ExecStartPost = "${lib.getExe' pkgs.kmod "modprobe"} -r msr";
+    };
+  };
+
   services.physlock.enable = true;
 
   services.dbus.implementation = "broker";
