e4043ffb1f31f236c5a4b1f90da9ae1bd99bef71
pancho horrillo <pancho@pancho.name>
2023-12-25 16:07:18 +0100

    feat(config): trim down locale settings

--- configuration.nix
+++ configuration.nix
@@ -130,16 +130,10 @@
   };
 
   i18n = {
-    defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "en_DK.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_DK.UTF-8";
-      LC_MONETARY = "en_IE.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_DK.UTF-8";
-      LC_TELEPHONE = "en_DK.UTF-8";
+      LC_MEASUREMENT = "en_DK.UTF-8"; # m, not in
+      LC_MONETARY = "en_IE.UTF-8"; # €, not $
+      LC_PAPER = "en_DK.UTF-8"; # DIN A4, not legal
       LC_TIME = "en_DK.UTF-8"; # yes, that means ISO-8601 ;-)
     };
   };
