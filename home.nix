0e6911cb4978e26c44d7e94f56cc6871744005ff
pancho horrillo <pancho@pancho.name>
2023-12-30 17:49:02 +0100

    refactor(home): extract openssh config to module

--- home.nix
+++ home.nix
@@ -11,6 +11,7 @@
     stylix.homeManagerModules.stylix
     autofirma-nix.homeManagerModules.default
     ./modules/hm/hyprland.nix
+    ./modules/hm/openssh.nix
   ];
 
   home.stateVersion = "23.11";
@@ -648,46 +649,7 @@
     pinentryFlavor = "gnome3";
   };
 
-  programs.ssh = {
-    enable = true;
-    addKeysToAgent = "yes";
-    controlMaster = "no";
-    controlPersist = "yes";
-    serverAliveInterval = 60;
-    extraConfig = ''
-      ExitOnForwardFailure yes
-      HostKeyAlgorithms    ssh-ed25519
-      IdentitiesOnly       yes
-      # IdentityFile         ~/.ssh/keys.d/id_ed25519-%r@%h
-      IdentityFile         ~/.ssh/keys.d/id_ed25519_openpgp_YubiKey_5C_Nano-%r@%h
-      IdentityFile         ~/.ssh/keys.d/id_ed25519_sk_rk_YubiKey_5C_NFC_#1-%r@%h
-      IdentityFile         ~/.ssh/keys.d/id_ed25519_sk_rk_YubiKey_5C_NFC_#2-%r@%h
-      IdentityFile         ~/.ssh/keys.d/id_ed25519_sk_rk_YubiKey_C_Bio_#1-%r@%h
-      IdentityFile         ~/.ssh/keys.d/id_ed25519_sk_rk_YubiKey_C_Bio_#2-%r@%h
-      SendEnv              LC_*
-      VisualHostKey        yes
-    '';
-    matchBlocks = {
-      "ubuntu* k8s-*" = lib.hm.dag.entryBefore ["*.vm"] {
-        user = "sysadmin";
-        identityFile = "~/.ssh/keys.d/id_ed25519-sysadmin@ubuntu";
-        extraOptions = {
-          GlobalKnownHostsFile = "/dev/null";
-          UserKnownHostsFile = "/dev/null";
-          StrictHostKeyChecking = "no";
-        };
-      };
-      "*.vm" = lib.hm.dag.entryAnywhere {
-        extraOptions = {
-          GlobalKnownHostsFile = "/dev/null";
-          UserKnownHostsFile = "/dev/null";
-          StrictHostKeyChecking = "no";
-        };
-        identityFile = "~/.ssh/keys.d/id_ed25519-wildcard.vm";
-        proxyCommand = "${pkgs.libressl}/bin/nc ( string replace .vm '' %h ) %p";
-      };
-    };
-  };
+  hm.openssh.enable = true;
 
   programs.ripgrep = {
     enable = true;
---
+++ modules/hm/openssh.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.hm.openssh;
+in {
+  options.hm.openssh = {
+    enable = lib.mkEnableOption "openssh";
+  };
+
+  config.programs.ssh = lib.mkIf cfg.enable {
+    enable = true;
+    addKeysToAgent = "yes";
+    controlMaster = "no";
+    controlPersist = "yes";
+    serverAliveInterval = 60;
+    extraConfig = ''
+      ExitOnForwardFailure yes
+      HostKeyAlgorithms    ssh-ed25519
+      IdentitiesOnly       yes
+      # IdentityFile         ~/.ssh/keys.d/id_ed25519-%r@%h
+      IdentityFile         ~/.ssh/keys.d/id_ed25519_openpgp_YubiKey_5C_Nano-%r@%h
+      IdentityFile         ~/.ssh/keys.d/id_ed25519_sk_rk_YubiKey_5C_NFC_#1-%r@%h
+      IdentityFile         ~/.ssh/keys.d/id_ed25519_sk_rk_YubiKey_5C_NFC_#2-%r@%h
+      IdentityFile         ~/.ssh/keys.d/id_ed25519_sk_rk_YubiKey_C_Bio_#1-%r@%h
+      IdentityFile         ~/.ssh/keys.d/id_ed25519_sk_rk_YubiKey_C_Bio_#2-%r@%h
+      SendEnv              LC_*
+      VisualHostKey        yes
+    '';
+    matchBlocks = {
+      "ubuntu* k8s-*" = lib.hm.dag.entryBefore ["*.vm"] {
+        user = "sysadmin";
+        identityFile = "~/.ssh/keys.d/id_ed25519-sysadmin@ubuntu";
+        extraOptions = {
+          GlobalKnownHostsFile = "/dev/null";
+          UserKnownHostsFile = "/dev/null";
+          StrictHostKeyChecking = "no";
+        };
+      };
+      "*.vm" = lib.hm.dag.entryAnywhere {
+        extraOptions = {
+          GlobalKnownHostsFile = "/dev/null";
+          UserKnownHostsFile = "/dev/null";
+          StrictHostKeyChecking = "no";
+        };
+        identityFile = "~/.ssh/keys.d/id_ed25519-wildcard.vm";
+        proxyCommand = "${pkgs.libressl}/bin/nc ( string replace .vm '' %h ) %p";
+      };
+    };
+  };
+}
