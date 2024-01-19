ea189a00ffb34ca4360e752957e53f80e6662aab
pancho horrillo <pancho@pancho.name>
2024-01-19 22:24:15 +0100

    refactor(home): extract fish et al. to module

--- home.nix
+++ home.nix
@@ -12,6 +12,7 @@
     ./modules/hm/fuzzel.nix
     ./modules/hm/foot.nix
     ./modules/hm/tmux.nix
+    ./modules/hm/fish.nix
     ./modules/hm/openssh.nix
     ./modules/hm/gnupg.nix
     ./modules/hm/stylix.nix
@@ -199,83 +200,7 @@
 
   programs.bash.enable = true;
 
-  programs.fish = {
-    enable = true;
-    shellAliases = {
-      e = "${
-        lib.getExe' config.programs.emacs.finalPackage "emacsclient"
-      } --no-wait --reuse-frame --alternate-editor=${lib.getExe pkgs.neovim}";
-    };
-  };
-  programs.starship = {
-    enable = true;
-    settings = {
-      hostname = {
-        ssh_only = false;
-      };
-      golang = {
-        symbol = " ";
-      };
-    };
-  };
-  programs.lsd = {
-    enable = true;
-    enableAliases = true;
-    settings = {
-      date = "relative";
-      header = true;
-      blocks = [
-        "permission"
-        "links"
-        # "inode"
-        "user"
-        "group"
-        "context"
-        "size"
-        "date"
-        "name"
-        # "git"
-      ];
-      ignore-globs = [
-        ".git"
-        ".hg"
-      ];
-    };
-  };
-  programs.eza = {
-    enable = false;
-    enableAliases = true;
-    extraOptions = [
-      "--group-directories-first"
-      "--group"
-      "--header"
-    ];
-    icons = true;
-  };
-  programs.bat.enable = true;
-  programs.fzf = {
-    enable = true;
-    tmux = {
-      enableShellIntegration = true;
-      shellIntegrationOptions = ["-d 40%"];
-    };
-  };
-  programs.zoxide.enable = true;
-  programs.thefuck.enable = true;
-  programs.jq.enable = true;
-
-  programs.lf = {
-    enable = true;
-    settings = {
-      icons = true;
-      sixel = true;
-    };
-  };
-
-  programs.yazi = {
-    enable = true;
-    enableFishIntegration = true;
-  };
+  hm.fish.enable = true;
 
   programs.git = {
     enable = true;
---
+++ modules/hm/fish.nix
@@ -0,0 +1,90 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.hm.fish;
+in {
+  options.hm.fish.enable = lib.mkEnableOption "fish";
+
+  config = lib.mkIf cfg.enable {
+    programs.fish = {
+      enable = true;
+      shellAliases = {
+        e = "${
+          lib.getExe' config.programs.emacs.finalPackage "emacsclient"
+        } --no-wait --reuse-frame --alternate-editor=${lib.getExe pkgs.neovim}";
+      };
+    };
+    programs.starship = {
+      enable = true;
+      settings = {
+        hostname = {
+          ssh_only = false;
+        };
+        golang = {
+          symbol = " ";
+        };
+      };
+    };
+    programs.lsd = {
+      enable = true;
+      enableAliases = true;
+      settings = {
+        date = "relative";
+        header = true;
+        blocks = [
+          "permission"
+          "links"
+          # "inode"
+          "user"
+          "group"
+          "context"
+          "size"
+          "date"
+          "name"
+          # "git"
+        ];
+        ignore-globs = [
+          ".git"
+          ".hg"
+        ];
+      };
+    };
+    programs.eza = {
+      enable = false;
+      enableAliases = true;
+      extraOptions = [
+        "--group-directories-first"
+        "--group"
+        "--header"
+      ];
+      icons = true;
+    };
+    programs.bat.enable = true;
+    programs.fzf = {
+      enable = true;
+      tmux = {
+        enableShellIntegration = true;
+        shellIntegrationOptions = ["-d 40%"];
+      };
+    };
+    programs.zoxide.enable = true;
+    programs.thefuck.enable = true;
+    programs.jq.enable = true;
+
+    programs.lf = {
+      enable = true;
+      settings = {
+        icons = true;
+        sixel = true;
+      };
+    };
+
+    programs.yazi = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+  };
+}
