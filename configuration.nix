327b738700393ac52e93e57f653c585ccea91944
pancho horrillo <pancho@pancho.name>
2024-01-20 12:04:39 +0100

    refactor(cfg): extract user to module

--- configuration.nix
+++ configuration.nix
@@ -27,6 +27,7 @@
     ./modules/traits/kmscon.nix
     ./modules/traits/greetd.nix
     ./modules/traits/locate.nix
+    ./modules/traits/user.nix
 
     {disabledModules = [(modulesPath + "/programs/chromium.nix")];}
     ./modules/programs/chromium.nix
@@ -92,20 +93,7 @@
     wireplumber.enable = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.mutableUsers = false;
-  users.groups."storage".members = [attrs.userName or "alice"];
-  users.users.${attrs.userName or "alice"} = {
-    isNormalUser = true;
-    description = attrs.userDesc or "Alice Q. User";
-    extraGroups = ["wheel" "libvirtd" "docker" "audio"];
-    shell = pkgs.fish;
-    initialPassword = "password";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhtv6KrJc04bydU2mj6j/V6g/g+RiY1+gTg9h4z3STm pancho"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOK1QiBQzjzVDZoyWwewN8U0B6QRn09dasbcyTI48dWL pancho@ipad"
-    ];
-  };
+  traits.user.enable = true;
 
   programs.hyprland.enable = true;
   programs.hyprland.xwayland.enable = true;
---
+++ modules/traits/user.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  attrs ? null,
+  ...
+}: let
+  cfg = config.traits.user;
+in {
+  options.traits.user.enable = lib.mkEnableOption "user";
+
+  config = lib.mkIf cfg.enable {
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    # users.mutableUsers = false;
+    users.groups."storage".members = [attrs.userName or "alice"];
+    users.users.${attrs.userName or "alice"} = {
+      isNormalUser = true;
+      description = attrs.userDesc or "Alice Q. User";
+      extraGroups = ["wheel" "libvirtd" "docker" "audio"];
+      shell = pkgs.fish;
+      initialPassword = "password";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhtv6KrJc04bydU2mj6j/V6g/g+RiY1+gTg9h4z3STm pancho"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOK1QiBQzjzVDZoyWwewN8U0B6QRn09dasbcyTI48dWL pancho@ipad"
+      ];
+    };
+  };
+}
