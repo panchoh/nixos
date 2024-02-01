d052e2ff5d27b62a0ddb1e810e2ac1b2e8d4b8ad
pancho horrillo <pancho@pancho.name>
2024-02-01 22:59:09 +0100

    feat(traits): add ansible

---
+++ modules/traits/ansible/hm/default.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.traits.ansible;
+in {
+  options.traits.ansible = {
+    enable = lib.mkEnableOption "ansible" // {default = true;};
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      ansible
+      ansible-lint
+      ansible-language-server
+      sshpass
+      neo-cowsay
+    ];
+  };
+}
