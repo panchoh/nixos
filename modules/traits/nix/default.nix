dab04c29c73ca11c9f90b2b9d102d39f07267b34
pancho horrillo <pancho@pancho.name>
2024-02-02 08:09:25 +0100

    fix(traits/nix): support non-flake tooling
    See https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry

--- modules/traits/nix/default.nix
+++ modules/traits/nix/default.nix
@@ -1,7 +1,7 @@
 {
   config,
   lib,
-  pkgs,
+  nixpkgs,
   ...
 }: let
   cfg = config.traits.nix;
@@ -11,15 +11,15 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    # NIX_PATH is still used by many useful tools, such as Doom Emacs,  so we
+    # set it to the same value as the one used by this flake.  Make `nix repl
+    # '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+    environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
+
     nix = {
       channel.enable = false;
 
-      registry = {
-        nixpkgs.to = {
-          type = "path";
-          path = pkgs.path;
-        };
-      };
+      registry.nixpkgs.flake = nixpkgs;
 
       settings = {
         # https://nixos.org/manual/nix/unstable/command-ref/conf-file
@@ -32,6 +32,8 @@ in {
           "flakes"
           "repl-flake" # TODO: remove once nix 1.19 percolates to NixOS
         ];
+        # https://github.com/NixOS/nix/issues/9574
+        nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
       };
 
       gc = {
