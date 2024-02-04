4d6065c674d302853472d37d1bdb706a75300db7
pancho horrillo <pancho@pancho.name>
2024-02-04 18:40:36 +0100

    style(traits/stylix): drop stale comments & hash

--- modules/traits/stylix/hm/default.nix
+++ modules/traits/stylix/hm/default.nix
@@ -23,25 +23,8 @@ in {
 
     image = pkgs.fetchurl {
       url = "https://github.com/NixOS/nixos-artwork/raw/master/wallpapers/nix-wallpaper-dracula.png";
-      sha256 = "sha256-SykeFJXCzkeaxw06np0QkJCK28e0k30PdY8ZDVcQnh4=";
+      hash = "sha256-SykeFJXCzkeaxw06np0QkJCK28e0k30PdY8ZDVcQnh4=";
     };
-
-    # https://www.reddit.com/r/NixOS/comments/3jqd2u/anyone_want_a_wallpaper/
-    # also check: https://github.com/NixOS/nixos-artwork/tree/master/wallpapers
-    # image = pkgs.fetchurl {
-    #   url = "http://reign-studios.com/wallpapers/nixos/wallpaper.svg";
-    #   sha256 = "sha256-vXbw39v0sA+aR/9Gg0NOPgL3QHuw0Wl+ACbn9VJ8Fyg=";
-    # };
-
-    # image = pkgs.fetchurl {
-    #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-    #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    # };
-
-    # image = pkgs.fetchurl {
-    #   url = "https://cdnb.artstation.com/p/assets/images/images/016/252/301/4k/grady-frederick-atlantis-garbageman-v2.jpg";
-    #   sha256 = "tAX6qTm1/7v/auvCHrmRswJsScNieSWpXV6TCBhRP7Y=";
-    # };
 
     fonts = {
       serif = {
