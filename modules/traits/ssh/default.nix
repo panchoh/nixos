e34e12d3cbf6f82ce08e373184f0b58792e680dc
pancho horrillo <pancho@pancho.name>
2024-02-19 07:39:23 +0100

    feat(traits/ssh): github.com pubkey to known_hosts

--- modules/traits/ssh/default.nix
+++ modules/traits/ssh/default.nix
@@ -12,6 +12,11 @@ in {
   config = lib.mkIf cfg.enable {
     programs.mosh.enable = true;
 
+    programs.ssh.knownHosts = {
+      # obtained by running `ssh-keyscan -t ed25519 github.com`
+      "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    };
+
     services.openssh = {
       enable = true;
       startWhenNeeded = true;
