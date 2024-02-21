748b92375f0e1800d8a6927c52fca7304526a8cf
pancho horrillo <pancho@pancho.name>
2024-02-21 22:00:24 +0100

    fix(traits/imv): disable until freeimage is fixed

--- modules/traits/imv/hm/default.nix
+++ modules/traits/imv/hm/default.nix
@@ -6,7 +6,7 @@
   cfg = config.traits.hm.imv;
 in {
   options.traits.hm.imv = {
-    enable = lib.mkEnableOption "imv" // {default = true;};
+    enable = lib.mkEnableOption "imv" // {default = false;}; # freeimage deemed insecure
   };
 
   config = lib.mkIf cfg.enable {
