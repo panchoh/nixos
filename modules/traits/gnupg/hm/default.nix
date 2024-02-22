d51d544531ba303d93223a003b15d5b8cd00f976
pancho horrillo <pancho@pancho.name>
2024-02-22 08:56:21 +0100

    fix(traits/gnupg): prevent conflict with pcscd
    (Verify shared mode by running pcsc_scan).
    
    Errors like these could be seen by running:
    $ journalctl --unit pcscd.service
    -- Boot 8564d4c10b09427fa108594deeda2492 --
    Feb 21 00:03:47 krypton systemd[1]: Started PC/SC Smart Card Daemon.
    Feb 21 00:05:21 krypton pcscd[6025]: 00000000 winscard.c:281:SCardConnect() Error Reader Exclusive
    Feb 21 00:05:21 krypton pcscd[6025]: 00062219 winscard.c:281:SCardConnect() Error Reader Exclusive
    Feb 21 00:05:21 krypton pcscd[6025]: 00062021 winscard.c:281:SCardConnect() Error Reader Exclusive
    Feb 21 00:05:27 krypton pcscd[6025]: 05138222 winscard.c:281:SCardConnect() Error Reader Exclusive
    Feb 21 00:05:27 krypton pcscd[6025]: 00062676 winscard.c:281:SCardConnect() Error Reader Exclusive
    Feb 21 00:05:27 krypton pcscd[6025]: 00061805 winscard.c:281:SCardConnect() Error Reader Exclusive
    Feb 21 00:10:08 krypton systemd[1]: Stopping PC/SC Smart Card Daemon...
    Feb 21 00:10:08 krypton systemd[1]: pcscd.service: Deactivated successfully.
    Feb 21 00:10:08 krypton systemd[1]: Stopped PC/SC Smart Card Daemon.

--- modules/traits/gnupg/hm/default.nix
+++ modules/traits/gnupg/hm/default.nix
@@ -14,7 +14,9 @@ in {
       enable = true;
       scdaemonSettings = {
         disable-ccid = true; # Play nice with yubikey https://ludovicrousseau.blogspot.com/2019/06/gnupg-and-pcsc-conflicts.html
+        pcsc-shared = true; # Play nice with the rest of OpenSC-based tools
         reader-port = "Yubico YubiKey CCID 00 00"; # Get this value with pcsc_scan
+        disable-application = "piv"; # Allow gpg-agent to cache the PIN between operations https://bugzilla.redhat.com/show_bug.cgi?id=2018237 https://dev.gnupg.org/T5436
       };
       homedir = "${config.xdg.dataHome}/gnupg";
     };
