d07594a12aa122cb0387a8b548707baa59a5dbd2
pancho horrillo <pancho@pancho.name>
2024-02-22 08:05:16 +0100

    fix(traits/yubikey): drop hack since fix is in
    See https://github.com/NixOS/nixpkgs/issues/280826

--- modules/traits/yubikey/default.nix
+++ modules/traits/yubikey/default.nix
@@ -11,12 +11,6 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      # FIXME: fix pcscd
-      # https://github.com/NixOS/nixpkgs/issues/280826
-      pcscliteWithPolkit.out
-    ];
-
     services = {
       pcscd.enable = true;
       udev.packages = [pkgs.yubikey-personalization];
