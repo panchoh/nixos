7dc84bdbe63377c388e639b13b90a96c402ddbb7
pancho horrillo <pancho@pancho.name>
2024-02-24 19:17:31 +0100

    refactor(traits/hardware): subsume traits/hw-cfg

--- modules/traits/hardware/default.nix
+++ modules/traits/hardware/default.nix
@@ -2,16 +2,35 @@
   config,
   lib,
   pkgs,
+  modulesPath,
+  box ? null,
   ...
 }: let
   cfg = config.traits.hardware;
 in {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
   options.traits.hardware = {
     enable = lib.mkEnableOption "hardware" // {default = true;};
   };
 
   config = lib.mkIf cfg.enable {
+    boot = {
+      initrd = {
+        availableKernelModules = ["xhci_pci" "ahci" "nvme" "sd_mod"];
+        kernelModules = [];
+      };
+      kernelModules = ["kvm-intel"];
+      extraModulePackages = [];
+    };
+
+    nixpkgs.hostPlatform = lib.mkDefault box.system or "x86_64-linux";
+
     hardware = {
+      cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
       logitech.wireless = {
         enable = true;
         enableGraphical = true;
