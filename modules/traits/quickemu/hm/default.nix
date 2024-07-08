378464da183624cd701ce4bc81b31466bdc436e8
pancho horrillo <pancho@pancho.name>
2024-07-08 07:52:28 +0200

    feat(traits/quickemu): install quick{emu,gui}

---
+++ modules/traits/quickemu/hm/default.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  box ? null,
+  ...
+}: let
+  cfg = config.traits.hm.quickemu;
+in {
+  options.traits.hm.quickemu = {
+    enable = lib.mkEnableOption "quickemu" // {default = box.isStation or false;};
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      pkgs.quickemu
+      pkgs.quickgui
+    ];
+  };
+}
