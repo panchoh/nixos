366c407a40443a21ae01c152053741dd173dd396
pancho horrillo <pancho@pancho.name>
2024-08-18 21:38:34 +0200

    refactor(modules/lib): mods can be grouped in dirs
    modules/traits/{foo,bar,baz}/{os,hm}
    
    can now be grouped in arbitrarily nested subdirectories:
    
    modules/traits/gui/{foo,bar}/{os,hm} and modules/traits/cli/baz/{os,hm}
    
    for instance.

--- modules/lib/module.nix
+++ modules/lib/module.nix
@@ -1,23 +1,21 @@
-inputs: subDir: let
-  inherit (builtins) filter pathExists readDir;
-  inherit (inputs.nixpkgs.lib) attrNames filterAttrs concatStringsSep optionalString;
+inputs: moduleFamily: let
+  inherit (builtins) readDir baseNameOf;
+  inherit (inputs.nixpkgs.lib) concatMap concatStringsSep mapAttrsToList;
   baseDir = ../traits;
+
+  traverse = family: path:
+    concatMap (
+      entry: let
+        inherit (entry) name type;
+      in
+        if type == "directory"
+        then traverse family (concatStringsSep "/" [path name])
+        else if name == "default.nix" && type == "regular" && (baseNameOf path == family)
+        then [(concatStringsSep "/" [path name])]
+        else []
+    ) (mapAttrsToList (name: type: {
+      inherit name type;
+    }) (readDir path));
 in {
-  imports = filter pathExists (
-    map (
-      name:
-        concatStringsSep "/" [
-          baseDir
-          name
-          (optionalString (subDir != null) subDir)
-          "default.nix"
-        ]
-    )
-    (
-      attrNames (
-        filterAttrs (_name: type: type == "directory")
-        (readDir baseDir)
-      )
-    )
-  );
+  imports = traverse moduleFamily baseDir;
 }
