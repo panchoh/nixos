73d6d4b6f0959c960407a70bf5f9c457017666ba
pancho horrillo <pancho@pancho.name>
2024-10-04 22:32:25 +0200

    feat(traits/fwupd): enable for all profiles

--- modules/traits/services/fwupd/os/default.nix
+++ modules/traits/services/fwupd/os/default.nix
@@ -7,7 +7,7 @@
   cfg = config.traits.os.fwupd;
 in {
   options.traits.os.fwupd = {
-    enable = lib.mkEnableOption "fwupd" // {default = box.isStation or false;};
+    enable = lib.mkEnableOption "fwupd" // {default = true;};
   };
 
   config = lib.mkIf cfg.enable {
