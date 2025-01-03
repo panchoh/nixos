13756840daeface14421654a88a9d3d795fe9126
pancho horrillo <pancho@pancho.name>
2025-01-03 23:55:23 +0100

    feat(traits/ghostty): enable

---
+++ modules/traits/gui/ghostty/hm/default.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  box ? null,
+  ...
+}: let
+  cfg = config.traits.hm.ghostty;
+in {
+  options.traits.hm.ghostty = {
+    enable = lib.mkEnableOption "ghostty" // {default = box.isStation or false;};
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.ghostty = {
+      enable = true;
+      enableFishIntegration = true;
+      installBatSyntax = true;
+      settings = {
+        window-decoration = false;
+      };
+    };
+  };
+}
