2ad17c88bf975feac6eb294b70d368e943cc77e4
pancho horrillo <pancho@pancho.name>
2025-03-26 18:32:11 +0100

    feat(traits/dosbox): add dosbox-staging

---
+++ modules/traits/applications/dosbox/hm/default.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  lib,
+  pkgs,
+  box ? null,
+  ...
+}: let
+  cfg = config.traits.hm.dosbox;
+in {
+  options.traits.hm.dosbox = {
+    enable = lib.mkEnableOption "DOSBox Staging" // {default = box.isStation or false;};
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [pkgs.dosbox-staging];
+  };
+}
