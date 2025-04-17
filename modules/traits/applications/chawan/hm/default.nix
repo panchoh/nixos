bcc04e7990e0de296bb4f9a7dc6882517e23c1bc
pancho horrillo <pancho@pancho.name>
2025-04-17 09:09:25 +0200

    feat(traits/chawan): init

---
+++ modules/traits/applications/chawan/hm/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.traits.hm.chawan;
+in {
+  options.traits.hm.chawan = {
+    enable = lib.mkEnableOption "chawan" // {default = true;};
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.chawan = {
+      enable = true;
+      settings = {
+        # https://git.sr.ht/~bptato/chawan/tree/HEAD/doc/config.md
+        buffer = {
+          images = true;
+          autofocus = true;
+        };
+        pager."C-k" = "() => pager.load('https://duckduckgo.com/?=')";
+      };
+    };
+  };
+}
