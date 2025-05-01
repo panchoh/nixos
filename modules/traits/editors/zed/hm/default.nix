2509c70534625f0f2beb44ba1945e6dce76e4fb4
pancho horrillo <pancho@pancho.name>
2025-05-01 05:30:56 +0200

    feat(traits/zed): first cut at doom keybindings

--- modules/traits/editors/zed/hm/default.nix
+++ modules/traits/editors/zed/hm/default.nix
@@ -16,6 +16,39 @@ in {
       extensions = [
         "nix"
       ];
+
+      userKeymaps = [
+        {
+          context = "VimControl && !menu";
+          bindings = {
+            "space c a" = "editor::ToggleCodeActions";
+            "space f f" = "file_finder::Toggle";
+            "space f s" = "workspace::Save";
+            "space f p" = "zed::OpenSettings";
+            "space o p" = "project_panel::ToggleFocus";
+            "space o t" = "terminal_panel::ToggleFocus";
+            "space q q" = "zed::Quit";
+            "space w 1" = ["workspace::ActivatePane" 0];
+            "space w 2" = ["workspace::ActivatePane" 1];
+            "space w 3" = ["workspace::ActivatePane" 2];
+            "space w 4" = ["workspace::ActivatePane" 3];
+            "space w 5" = ["workspace::ActivatePane" 4];
+            "space w 6" = ["workspace::ActivatePane" 5];
+            "space w 7" = ["workspace::ActivatePane" 6];
+            "space w 8" = ["workspace::ActivatePane" 7];
+            "space w 9" = ["workspace::ActivatePane" 8];
+            "space w c" = "pane::CloseActiveItem";
+            "space w d" = "pane::CloseActiveItem";
+            "space w h" = "workspace::ActivatePaneLeft";
+            "space w j" = "workspace::ActivatePaneDown";
+            "space w l" = "workspace::ActivatePaneRight";
+            "space w k" = "workspace::ActivatePaneUp";
+            "space w s" = "pane::SplitHorizontal";
+            "space w v" = "pane::SplitVertical";
+          };
+        }
+      ];
+
       userSettings = {
         auto_update = false;
         autosave = "on_focus_change";
