a11cf8ec5af980e836e2af2d0c3dd36bd63df09d
pancho horrillo <pancho@pancho.name>
2025-05-08 13:31:51 +0200

    refactor(traits/gnupg): wayprompt config gen
    Leverage `pkgs.formats.ini`.
    Can now handle colour values (RGB/RGBA).

--- modules/traits/tools/gnupg/hm/default.nix
+++ modules/traits/tools/gnupg/hm/default.nix
@@ -6,23 +6,39 @@
   ...
 }: let
   cfg = config.traits.hm.gnupg;
-  inherit (lib.generators) toINI mkKeyValueDefault mkValueStringDefault;
+
+  # Matches 6-hex-digit RGB or 8-hex-digit RGBA values
+  colourHexPattern = "^[0-9a-fA-F]{6}([0-9a-fA-F]{2})?$";
+
+  isColourHex = str: builtins.match colourHexPattern str != null;
+
+  iniFormat = pkgs.formats.ini {
+    mkKeyValue = lib.generators.mkKeyValueDefault {
+      mkValueString = v:
+        if lib.isString v
+        then
+          (
+            if isColourHex v
+            then "0x${lib.strings.toUpper v};"
+            else ''"${v}";''
+          )
+        else lib.generators.mkValueStringDefault {} v + ";";
+    } " = ";
+  };
 in {
   options.traits.hm.gnupg = {
     enable = lib.mkEnableOption "gnupg" // {default = box.isStation or false;};
   };
 
   config = lib.mkIf cfg.enable {
-    home.file."${config.xdg.configHome}/wayprompt/config.ini".text =
-      toINI {
-        mkKeyValue = mkKeyValueDefault {
-          mkValueString = v: mkValueStringDefault {} v + ";";
-        } " = ";
-      } {
-        general = {
-          pin-square-amount = 32;
-        };
+    home.file."${config.xdg.configHome}/wayprompt/config.ini".source = iniFormat.generate "config.ini" {
+      general = {
+        pin-square-amount = 32;
       };
+      colours = {
+        background = "ffffffaa";
+      };
+    };
 
     programs.gpg = {
       enable = true;
