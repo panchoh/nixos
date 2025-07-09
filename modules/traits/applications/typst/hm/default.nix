eb8bf614d30fc5fe2cbfebdc8c6043fa7b11622b
pancho horrillo <pancho@pancho.name>
2025-07-09 10:39:13 +0200

    feat(traits/typst): add Typst

---
+++ modules/traits/applications/typst/hm/default.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  box ? null,
+  ...
+}: let
+  cfg = config.traits.hm.typst;
+in {
+  options.traits.hm.typst = {
+    enable = lib.mkEnableOption "Typst" // {default = box.isStation or false;};
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      pkgs.typst
+      pkgs.typstyle
+      pkgs.tinymist
+    ];
+  };
+}
