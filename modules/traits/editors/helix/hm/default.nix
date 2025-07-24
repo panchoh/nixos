16e150592b3dcd7a37d09dcd1bf306d8d6dde41c
pancho horrillo <pancho@pancho.name>
2025-07-24 10:52:50 +0200

    feat(traits/helix): tweak settings

--- modules/traits/editors/helix/hm/default.nix
+++ modules/traits/editors/helix/hm/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   box ? null,
   ...
 }:
@@ -9,12 +10,39 @@ let
 in
 {
   options.traits.hm.helix = {
-    enable = lib.mkEnableOption "helix" // {
+    enable = lib.mkEnableOption "Helix" // {
       default = box.isStation or false;
     };
   };
 
   config = lib.mkIf cfg.enable {
-    programs.helix.enable = true;
+    programs.helix = {
+      enable = true;
+      settings = {
+        editor.cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+      };
+      languages.language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter.command = lib.getExe pkgs.nixfmt;
+        }
+        {
+          name = "go";
+          auto-format = true;
+          formatter.command = lib.getExe pkgs.gofumpt;
+        }
+      ];
+      settings = {
+        editor = {
+          line-number = "relative";
+          lsp.display-messages = true;
+        };
+      };
+    };
   };
 }
