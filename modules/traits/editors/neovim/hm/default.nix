d99c86e285c84a40a15093de228a23b3adfe0270
pancho horrillo <pancho@pancho.name>
2025-07-31 17:53:17 +0200

    feat(traits/neovim): rebase onto NVF

--- modules/traits/editors/neovim/hm/default.nix
+++ modules/traits/editors/neovim/hm/default.nix
@@ -2,7 +2,8 @@
   config,
   lib,
   pkgs,
-  nixvim,
+  nvf,
+  box ? null,
   ...
 }:
 let
@@ -10,175 +11,206 @@ let
 in
 {
   imports = [
-    nixvim.homeModules.nixvim
-    ./disable_keys.nix
+    nvf.homeManagerModules.default
     ./neovide.nix
   ];
 
   options.traits.hm.neovim = {
-    enable = lib.mkEnableOption "neovim" // {
-      default = true;
+    enable = lib.mkEnableOption "Neovim" // {
+      default = box.isStation or false;
     };
   };
 
   config = lib.mkIf cfg.enable {
-    programs.nixvim = {
+
+    stylix.targets.nvf.enable = false;
+
+    home.packages = [
+      (pkgs.writeShellApplication {
+        name = "nvf-pristine";
+        runtimeInputs = [ pkgs.coreutils ];
+        text = ''
+          echo 'Cleaning NVF Neovim state…'
+          rm -rfv ~/.cache/{nvf,nvim} ~/.local/state/{nvf,nvim} ~/.local/share/{nvf,nvim}
+        '';
+      })
+    ];
+
+    programs.nvf = {
       enable = true;
-      nixpkgs.useGlobalPackages = true;
 
-      viAlias = true;
-      vimAlias = true;
+      settings = {
+        vim = {
+          autocomplete.blink-cmp.enable = true;
 
-      plugins.lualine.enable = true;
-
-      performance.byteCompileLua.enable = true;
-
-      luaLoader.enable = true;
-
-      opts = {
-        autoread = true;
-        updatetime = 100; # Faster completion
-        spell = false;
-        number = true;
-        relativenumber = true;
-        wrap = false;
-        tabstop = 4;
-        shiftwidth = 4;
-        expandtab = true;
-        signcolumn = "yes";
-        swapfile = false;
-        winborder = "rounded";
-      };
-
-      globals = {
-        mapleader = " ";
-        maplocalleader = " ";
-        termguicolors = true;
-      };
-
-      filetype.extension.typ = "typst";
-
-      plugins = {
-        cmp.enable = true;
-
-        nix.enable = true;
-
-        lsp = {
-          enable = true;
-          inlayHints = true;
-          servers.gopls.enable = true;
-        };
-
-        efmls-configs = {
-          enable = true;
-          setup = {
-            bash = {
-              formatter = "shfmt";
-              linter = "shellcheck";
+          binds = {
+            whichKey.enable = true;
+            hardtime-nvim = {
+              enable = true;
+              setupOpts.disabled_keys =
+                lib.attrsets.genAttrs
+                  [
+                    "<Insert>"
+                    "<Del>"
+                    "<Home>"
+                    "<End>"
+                    "<PageUp>"
+                    "<PageDown>"
+                  ]
+                  (_key: [
+                    ""
+                    "i"
+                  ]);
             };
           };
-        };
 
-        treesitter = {
-          enable = true;
+          # enableLuaLoader = true;
 
-          nixvimInjections = true;
+          bell = "visual";
 
-          settings = {
-            highlight.enable = true;
-            indent.enable = true;
-          };
-        };
-
-        treesitter-refactor = {
-          enable = true;
-          highlightDefinitions = {
+          clipboard = {
             enable = true;
-            # Set to false if you have an `updatetime` of ~100.
-            clearOnCursorMove = false;
+            providers.wl-copy.enable = true;
           };
+
+          comments.comment-nvim.enable = true;
+
+          extraPackages = [ pkgs.ripgrep ];
+
+          filetree.neo-tree = {
+            enable = true;
+            setupOpts = {
+              enable_cursor_hijack = true;
+              follow_current_file.enabled = true;
+              use_libuv_file_watcher = true;
+            };
+          };
+
+          globals = {
+            mapleader = " ";
+            maplocalleader = " ";
+            termguicolors = true;
+          };
+
+          keymaps = [
+            {
+              action = "<Nop>";
+              desc = "Leader";
+              key = "<Space>";
+              mode = "n";
+            }
+            {
+              action = "<Cmd>nohlsearch<Enter>";
+              desc = "Clear search results";
+              key = "<Esc>";
+              mode = "n";
+            }
+            {
+              action = "<Cmd>Pick help<Enter>";
+              desc = "Search help";
+              key = "<Leader>hh";
+              mode = "n";
+            }
+            {
+              action = "<Cmd>Pick files<Enter>";
+              desc = "Search files";
+              key = "<Leader><Leader>";
+              mode = "n";
+            }
+            {
+              action = "<Cmd>Pick grep_live<Enter>";
+              desc = "Search file contents";
+              key = "<Leader>/";
+              mode = "n";
+            }
+            {
+              action = "<Cmd>Neotree<Enter>";
+              desc = "Neo-tree";
+              key = "<Leader>op";
+              mode = "n";
+            }
+            {
+              action = "vim.lsp.buf.rename";
+              desc = "LSP rename";
+              key = "<Leader>cr";
+              lua = true;
+              mode = "n";
+            }
+          ];
+
+          languages = {
+            bash.enable = true;
+            clang.enable = true;
+            go.enable = true;
+            go.dap.enable = true;
+            go.format.type = "gofumpt";
+            lua.enable = true;
+            nix.enable = true;
+            typst.enable = true;
+          };
+
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+            inlayHints.enable = true;
+            lightbulb.enable = true;
+          };
+
+          mini.pick.enable = true;
+
+          options = {
+            # Tab handling and autoindenting
+            autoindent = true;
+            tabstop = 4;
+            shiftwidth = 4;
+            expandtab = true;
+
+            # Search
+            incsearch = true;
+            ignorecase = true;
+            smartcase = true;
+
+            # Line numbering
+            number = true;
+            relativenumber = true;
+
+            autoread = true;
+            updatetime = 100;
+            spell = false;
+            wrap = false;
+            signcolumn = "yes";
+            swapfile = false;
+            winborder = "rounded";
+          };
+
+          statusline.lualine.enable = true;
+
+          theme = {
+            enable = true;
+            name = "dracula";
+            style = "dark";
+          };
+
+          treesitter = {
+            enable = true;
+            fold = true;
+          };
+
+          ui = {
+            borders.enable = true;
+            breadcrumbs.enable = true;
+            noice.enable = true;
+          };
+
+          utility = {
+            direnv.enable = true;
+            preview.glow.enable = true;
+          };
+
+          viAlias = true;
+          vimAlias = true;
         };
-
-        hmts.enable = true;
       };
-
-      keymaps = [
-        {
-          action = "<CMD>Pick help<Enter>";
-          key = "<leader>h";
-        }
-        {
-          action = "<CMD>Pick files<Enter>";
-          key = "<leader>f";
-        }
-        {
-          action = "<CMD>Pick grep_live<Enter>";
-          key = "<leader>/";
-        }
-      ];
-
-      lsp.keymaps = [
-        {
-          key = "gd";
-          lspBufAction = "definition";
-        }
-        {
-          key = "gD";
-          lspBufAction = "references";
-        }
-        {
-          key = "gt";
-          lspBufAction = "type_definition";
-        }
-        {
-          key = "gi";
-          lspBufAction = "implementation";
-        }
-        {
-          key = "K";
-          lspBufAction = "hover";
-        }
-        # {
-        #   action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=-1, float=true }) end";
-        #   key = "<leader>k";
-        # }
-        # {
-        #   action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=1, float=true }) end";
-        #   key = "<leader>j";
-        # }
-        {
-          action = "<CMD>LspStop<Enter>";
-          key = "<leader>lx";
-        }
-        {
-          action = "<CMD>LspStart<Enter>";
-          key = "<leader>ls";
-        }
-        {
-          action = "<CMD>LspRestart<Enter>";
-          key = "<leader>lr";
-        }
-        # {
-        #   action = lib.nixvim.mkRaw "require('telescope.builtin').lsp_definitions";
-        #   key = "gd";
-        # }
-        {
-          action = "<CMD>Lspsaga hover_doc<Enter>";
-          key = "K";
-        }
-      ];
-
-      extraConfigLua = ''
-        require('mini.pick').setup()
-      '';
-
-      extraPlugins = [
-        pkgs.vimPlugins.mini-pick
-      ];
-
-      extraPackages = [
-        pkgs.ripgrep
-      ];
     };
   };
 }
