a2d460f6dfe0252d819165073343139bc32d3d4a
pancho horrillo <pancho@pancho.name>
2025-07-31 10:36:49 +0200

    feat(traits/neovim): misc tweaks and configs

--- modules/traits/editors/neovim/hm/default.nix
+++ modules/traits/editors/neovim/hm/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   nixvim,
   ...
 }:
@@ -20,7 +21,6 @@ in
     };
   };
 
-  # TODO: Import config from https://github.com/GaetanLepage/nix-config/blob/master/home/modules/tui/neovim/options.nix
   config = lib.mkIf cfg.enable {
     programs.nixvim = {
       enable = true;
@@ -36,9 +36,27 @@ in
       luaLoader.enable = true;
 
       opts = {
+        autoread = true;
         updatetime = 100; # Faster completion
         spell = false;
+        number = true;
+        relativenumber = true;
+        wrap = false;
+        tabstop = 4;
+        shiftwidth = 4;
+        expandtab = true;
+        signcolumn = "yes";
+        swapfile = false;
+        winborder = "rounded";
       };
+
+      globals = {
+        mapleader = " ";
+        maplocalleader = " ";
+        termguicolors = true;
+      };
+
+      filetype.extension.typ = "typst";
 
       plugins = {
         cmp.enable = true;
@@ -48,6 +66,7 @@ in
         lsp = {
           enable = true;
           inlayHints = true;
+          servers.gopls.enable = true;
         };
 
         efmls-configs = {
@@ -82,6 +101,84 @@ in
 
         hmts.enable = true;
       };
+
+      keymaps = [
+        {
+          action = "<CMD>Pick help<Enter>";
+          key = "<leader>h";
+        }
+        {
+          action = "<CMD>Pick files<Enter>";
+          key = "<leader>f";
+        }
+        {
+          action = "<CMD>Pick grep_live<Enter>";
+          key = "<leader>/";
+        }
+      ];
+
+      lsp.keymaps = [
+        {
+          key = "gd";
+          lspBufAction = "definition";
+        }
+        {
+          key = "gD";
+          lspBufAction = "references";
+        }
+        {
+          key = "gt";
+          lspBufAction = "type_definition";
+        }
+        {
+          key = "gi";
+          lspBufAction = "implementation";
+        }
+        {
+          key = "K";
+          lspBufAction = "hover";
+        }
+        # {
+        #   action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=-1, float=true }) end";
+        #   key = "<leader>k";
+        # }
+        # {
+        #   action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=1, float=true }) end";
+        #   key = "<leader>j";
+        # }
+        {
+          action = "<CMD>LspStop<Enter>";
+          key = "<leader>lx";
+        }
+        {
+          action = "<CMD>LspStart<Enter>";
+          key = "<leader>ls";
+        }
+        {
+          action = "<CMD>LspRestart<Enter>";
+          key = "<leader>lr";
+        }
+        # {
+        #   action = lib.nixvim.mkRaw "require('telescope.builtin').lsp_definitions";
+        #   key = "gd";
+        # }
+        {
+          action = "<CMD>Lspsaga hover_doc<Enter>";
+          key = "K";
+        }
+      ];
+
+      extraConfigLua = ''
+        require('mini.pick').setup()
+      '';
+
+      extraPlugins = [
+        pkgs.vimPlugins.mini-pick
+      ];
+
+      extraPackages = [
+        pkgs.ripgrep
+      ];
     };
   };
 }
