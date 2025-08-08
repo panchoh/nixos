c197ce0a5ad0759bc1e26bf4892aa556e435ee9a
pancho horrillo <pancho@pancho.name>
2025-08-08 08:52:22 +0200

    refactor(lib/nixfmt-tree): cleanup

--- lib/nixfmt-tree.nix
+++ lib/nixfmt-tree.nix
@@ -1,12 +1,13 @@
 inputs:
 let
-  inherit (inputs) nixpkgs;
-  inherit (inputs.nixpkgs.lib) unique catAttrs;
   inherit (inputs.self.lib) boxen;
+  inherit (builtins) catAttrs listToAttrs;
+  inherit (inputs.nixpkgs.lib.lists) unique;
+  inherit (inputs.nixpkgs.lib.attrsets) nameValuePair;
+  inherit (inputs.nixpkgs) legacyPackages;
 in
-(builtins.listToAttrs (
-  map (system: {
-    name = system;
-    value = nixpkgs.legacyPackages.${system}.nixfmt-tree;
-  }) (unique (catAttrs "system" boxen))
-))
+boxen
+|> catAttrs "system"
+|> unique
+|> map (system: nameValuePair system legacyPackages.${system}.nixfmt-tree)
+|> listToAttrs
