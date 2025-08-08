de1d3c9b8504de5024dc1e8713d2f574b1c4aa52
pancho horrillo <pancho@pancho.name>
2025-08-08 15:20:27 +0200

    refactor(lib/nixfmt-tree): leverage lib.systems

--- lib/nixfmt-tree.nix
+++ lib/nixfmt-tree.nix
@@ -1,12 +1,9 @@
 inputs:
 let
-  inherit (inputs.self.lib) boxen;
-  inherit (builtins) catAttrs;
-  inherit (inputs.nixpkgs.lib.lists) unique;
-  inherit (inputs.nixpkgs.lib.trivial) flip;
+  inherit (inputs.self.lib) systems;
   inherit (inputs.nixpkgs.lib.attrsets) genAttrs;
   inherit (inputs.nixpkgs) legacyPackages;
 
-  systems = boxen |> catAttrs "system" |> unique;
+  nixFmtTreeForSystem = system: legacyPackages.${system}.nixfmt-tree;
 in
-systems |> flip genAttrs (system: legacyPackages.${system}.nixfmt-tree)
+genAttrs systems nixFmtTreeForSystem
