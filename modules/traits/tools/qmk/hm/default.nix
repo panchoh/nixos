15ee5cd3ede011a6f34c8490a59e737429b03665
pancho horrillo <pancho@pancho.name>
2025-08-09 06:56:36 +0200

    fix(traits/qmk): don’t use on servers

--- modules/traits/tools/qmk/hm/default.nix
+++ modules/traits/tools/qmk/hm/default.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  box ? null,
   ...
 }:
 let
@@ -10,7 +11,7 @@ in
 {
   options.traits.hm.qmk = {
     enable = lib.mkEnableOption "QMK" // {
-      default = true;
+      default = box.isStation or false;
     };
   };
 
