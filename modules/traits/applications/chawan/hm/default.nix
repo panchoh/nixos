cfcf6d2885d2d491ff999015b7039bc3361214c4
pancho horrillo <pancho@pancho.name>
2025-08-09 05:37:38 +0200

    chore(traits/chawan): don’t install on servers

--- modules/traits/applications/chawan/hm/default.nix
+++ modules/traits/applications/chawan/hm/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  box ? null,
   ...
 }:
 let
@@ -9,7 +10,7 @@ in
 {
   options.traits.hm.chawan = {
     enable = lib.mkEnableOption "Chawan" // {
-      default = true;
+      default = box.isStation or false;
     };
   };
 
