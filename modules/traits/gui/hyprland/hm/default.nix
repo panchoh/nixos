31e9aace02f46bad02a734614ed28f6d48b1cdd8
pancho horrillo <pancho@pancho.name>
2025-08-22 07:40:06 +0200

    chore(traits/hyprland): reduce audio volume delta

--- modules/traits/gui/hyprland/hm/default.nix
+++ modules/traits/gui/hyprland/hm/default.nix
@@ -336,10 +336,12 @@ in
         bindl = [
           # Multimedia
           "     , XF86AudioMute,        exec, wpctl set-mute        @DEFAULT_AUDIO_SINK@   toggle"
-          "     , XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@   4%+"
-          "     , XF86AudioLowerVolume, exec, wpctl set-volume      @DEFAULT_AUDIO_SINK@   4%-"
-          "SHIFT, XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@   1%+"
-          "SHIFT, XF86AudioLowerVolume, exec, wpctl set-volume      @DEFAULT_AUDIO_SINK@   1%-"
+          "     , XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@   3%+"
+          "     , XF86AudioLowerVolume, exec, wpctl set-volume      @DEFAULT_AUDIO_SINK@   3%-"
+          "ALT  , XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@   1%+"
+          "ALT  , XF86AudioLowerVolume, exec, wpctl set-volume      @DEFAULT_AUDIO_SINK@   1%-"
+          "SHIFT, XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@   6%+"
+          "SHIFT, XF86AudioLowerVolume, exec, wpctl set-volume      @DEFAULT_AUDIO_SINK@   6%-"
           "     , XF86AudioPrev,        exec, playerctl previous"
           "     , XF86AudioPlay,        exec, playerctl play-pause"
           "     , XF86AudioNext,        exec, playerctl next"
